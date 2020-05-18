13414979e6d8963a2356de790fa159e7d3617516
Chaitu Sri <chaitu8223@outlook.com>
2020-05-19 01:31:57 +0530

    added dbset for type of page regards  model class

---
+++ Models/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+
+        public int Slug { get; set; }
+
+        public int Content { get; set; }
+        public int Sorting { get; set; }
+
+
+
+    }
+}
